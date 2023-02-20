a3b3639f2fb3e860c4cf8a64f914094e4b752113
Mark Hahn <mark@hahnca.com>
2023-02-20 15:23:35 -0800

    firt commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  #hdr(style=`margin:0; 
+              font-weight:bold; 
+              font-size:2em;`) JSS
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+
+</script>
