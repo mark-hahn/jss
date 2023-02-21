12d155b15e51cd8faabd9be7c92bb6156fcf9697
Mark Hahn <mark@hahnca.com>
2023-02-20 16:32:45 -0800

    added favicon

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 div
-  #hdr(style=`margin:0; 
-              font-weight:bold; 
-              font-size:2em;`) JSS
+  #hdr(style="width:100%; background-color:#ccc; ")
+    div(style="margin:3px 10px; display:inline-block;width:100%")
+      #lbl TV Series
+      input(v-model="searchStr" @input="select"
+            style="border:1px solid black; width:100px;")
 
 </template>
 
