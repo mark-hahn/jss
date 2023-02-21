95b2680b5a10f6adcecf3eb3e1d79d5f1675f1a6
Mark Hahn <mark@hahnca.com>
2023-02-20 20:05:24 -0800

    flex hdr

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,12 @@ div
   #hdr(style=`border: 2px solid black; width:100%;
               position: fixed; left: 0; top: 0;
               background-color:yellow`)
-    div(style=`margin:0 1em; display:inline-block; width:100%;
-              display:flex;`)
+    div(style=`padding:0 1em; width:100%; display:flex;`)
       #lbl(style=`font-weight:bold; font-size:1.5em;
-                  flex-basis:5em;`) JSS
+                  flex-basis:2.5em;`) JSS
       input(v-model="topExpr" @input="select"
-            style=`border:2px solid black;
-                  position:relative; bottom:.15em;
-                  font-size:1.2em;
-                  flex-grow:1;`)
+            style=`border:1.5px solid black; height:1.5em;
+                  font-size:1.2em; flex-grow:1; margin-top:.25em;`)
 </template>
 
 <script>
