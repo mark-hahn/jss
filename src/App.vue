a3c85bcf66f990f936f760383f45afe5a7ad5cda
Mark Hahn <mark@hahnca.com>
2023-02-20 20:13:35 -0800

    changed jss to ssj

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,27 @@ div
               position: fixed; left: 0; top: 0;
               background-color:yellow`)
     div(style=`padding:0 1em; width:100%; display:flex;`)
-      #lbl(style=`font-weight:bold; font-size:1.5em;
-                  flex-basis:2.5em;`) JSS
+      div(style=`font-weight:bold; font-size:1.5em;
+                  flex-basis:2.5em;`) SSJ
       input(v-model="topExpr" @input="select"
             style=`border:1.5px solid black; height:1.5em;
-                  font-size:1.2em; flex-grow:1; margin-top:.25em;`)
+                   font-size:1.2em; flex-grow:1; margin-top:.25em;`)
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 
+export default {
+  name: "App",
+  
+  components: { },
+
+  data() {
+  },
+
+  methods: {
+  }
+}
 
 </script>
